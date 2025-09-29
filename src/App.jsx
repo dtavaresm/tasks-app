aba75ecec43c0c20358e5fd209ecb504abe65256
dtavaresm <tavares.machado.debora@gmail.com>
2025-09-29 13:56:22 +0100

    feat(tasks): create TasksPage component for task management and pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Container, Typography } from '@mui/material';
+import TasksPage from './components/TasksPage';
 
 function App() {
   return (
     <Container>
-      <Typography variant="h1" sx={{ fontWeight: '900', textAlign: 'center', color: 'primary.dark' }}>
+      <Typography variant="h1" sx={{ fontWeight: '900', textAlign: 'center', color: 'primary.dark', my: 4 }}>
         New App
       </Typography>
+      <TasksPage />
     </Container>
   )
 }
---
+++ src/components/TasksPage.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { Alert, Box, CircularProgress, IconButton, Pagination, Stack } from "@mui/material";
+import RefreshRoundedIcon from '@mui/icons-material/RefreshRounded';
+import { Task } from "../types";
+import TasksList from "./TasksList";
+
+const BASE_URL = "http://localhost:8080/api";
+
+export default function TasksPage() {
+    const [error, setError] = useState<Error | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const itemsPerPage = 5;
+    const paginationCount = Math.ceil(tasks.length / itemsPerPage);
+
+    const fetchTasks = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch(`${BASE_URL}/tasks`);
+            const data = await response.json();
+            setTasks(data);
+        } catch (error) {
+            setError(error instanceof Error ? error : new Error("Error"));
+        }
+        finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => { fetchTasks(); }, []);
+
+    if (isLoading) {
+        return (
+            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", maxHeight: "100vh" }}>
+                <CircularProgress size={80} sx={{ color: 'secondary.main' }} />
+            </Box>
+        );
+    }
+
+    if (error) {
+        return (
+            <Alert severity="error" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+                Something went wrong. Please try again.
+                <IconButton aria-label="delete" sx={{ ml: 2 }}>
+                    <RefreshRoundedIcon sx={{ color: 'primary.main' }} onClick={fetchTasks} />
+                </IconButton>
+            </Alert>
+        )
+    }
+
+    return (
+        <>
+            <TasksList fetchedData={tasks} currentPage={currentPage} itemsPerPage={itemsPerPage} />
+
+            <Stack spacing={2} sx={{ alignItems: 'center', mt: 4, height: 4 }}>
+                {paginationCount > 1 && (
+                    <Pagination
+                        count={paginationCount}
+                        page={currentPage}
+                        onChange={(_, page) => setCurrentPage(page)}
+                        color="primary"
+                    />
+                )}
+            </Stack>
+        </>
+    )
+}
