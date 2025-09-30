2901a6290c0b559ee5c093133b6312c3b2e3de04
dtavaresm <tavares.machado.debora@gmail.com>
2025-09-30 02:15:45 +0100

    feat(layout): restructure App component to include Menu and TasksPage in a responsive grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { Container, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import TasksPage from './components/TasksPage';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <Container>
-      <Typography variant="h1" sx={{ fontWeight: '900', textAlign: 'center', color: 'primary.dark', my: 4 }}>
+      <Typography variant="h1" sx={{ fontWeight: '900', textAlign: 'center', color: 'primary.dark', my: 1 }}>
         New App
       </Typography>
-      <TasksPage />
+      <Box sx={{
+        display: 'grid',
+        gridTemplateColumns: {
+          xs: "1fr",
+          sm: "0.2fr 1fr"
+        },
+        gap: 2
+      }}>
+        <Menu />
+        <TasksPage />
+      </Box>
     </Container>
   )
 }
