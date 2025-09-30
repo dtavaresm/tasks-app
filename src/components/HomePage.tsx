aeef7d5bdcdd57ca6249465f1e64f326282d1ed4
dtavaresm <tavares.machado.debora@gmail.com>
2025-09-30 14:08:12 +0100

    feat(HomePage): restructure HomePage component layout and add welcome message

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,15 @@
-
+import { Box, Typography } from '@mui/material';
 
 export default function HomePage() {
 
     return (
-        <>
-
-        </>
+        <Box sx={{ width: 'stretch' }}>
+            <Typography variant="body1" sx={{ fontWeight: '600', color: 'primary.dark', mb: 2 }}>
+                Welcome to the Task Manager App
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'primary.dark', textAlign: 'justify' }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu ipsum ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper elementum tellus, sed viverra diam rutrum vitae. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam vestibulum erat nec erat viverra iaculis. Etiam sed efficitur nulla. In hac habitasse platea dictumst.
+            </Typography>
+        </Box>
     )
 }
