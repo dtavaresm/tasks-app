fb4bf1fd3483d486a7733111472395bfd01154ba
dtavaresm <tavares.machado.debora@gmail.com>
2025-09-30 16:00:32 +0100

    feat(TasksList): enhance layout with responsive styles and overflow handling

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -14,7 +14,19 @@ export default function TasksList({ fetchedData, currentPage, itemsPerPage }: Ta
     const currentData = fetchedData.slice(firstIndex, lastIndex);
 
     return (
-        <Box sx={{ display: 'flex', gap: 2, flexDirection: 'column', width: 'stretch'}}>
+        <Box sx={{
+            display: 'flex',
+            gap: 2,
+            flexDirection: 'column',
+            width: 'stretch',
+            height: '70dvh',
+            overflowY: 'auto',
+            p:
+            {
+                xs: '1% 1% 2%',
+                sm: '1% 2% 2%'
+            },
+        }}>
             {currentData.map((task) => (
                 <TaskCard key={task.id} task={task} />
             ))}
