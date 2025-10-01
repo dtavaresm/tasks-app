31a32e42b2bf961e23558acbcb91b89527e9fd43
dtavaresm <tavares.machado.debora@gmail.com>
2025-10-01 20:04:03 +0100

    refactor(MenuItem): move sx styling to StyledPaper, StyledIconButton and  StyledMenuItem and add NavMenuItem wrapper

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,58 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, NavLinkProps } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import HomeIcon from '@mui/icons-material/Home';
 import TaskIcon from '@mui/icons-material/Task';
-import { Box, Drawer, IconButton, MenuItem, MenuList, Paper, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { styled } from "@mui/material/styles";
+import { Box, Drawer, IconButton, MenuItem, MenuItemProps, MenuList, Paper, Typography, useMediaQuery, useTheme } from '@mui/material';
+
+const StyledPaper = styled(Paper)(() => ({
+    borderRadius: 0,
+    height: 'fit-content',
+    padding: 0
+}));
+
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+    color: theme.palette.primary.main,
+    position: 'fixed',
+    top: theme.spacing(1),
+    left: theme.spacing(4),
+    backgroundColor: theme.palette.background.paper
+}));
+
+const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
+    display: "flex",
+    flexFlow: "row nowrap",
+    alignItems: "center",
+    gap: theme.spacing(1),
+    padding: theme.spacing(1),
+    textDecoration: 'none',
+    color: theme.palette.primary.main,
+    "& .MuiTypography-root": {
+        fontWeight: 600,
+        fontSize: theme.typography.body1.fontSize,
+        lineHeight: theme.typography.body1.lineHeight,
+        color: 'inherit',
+    },
+    ".active &": {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.light
+    },
+    [theme.breakpoints.down('sm')]: { 
+        paddingLeft: theme.spacing(4), 
+    }
+}));
+
+type NavMenuItemProps = MenuItemProps & NavLinkProps;
+
+export const NavMenuItem = ({ to, ...props }: NavMenuItemProps) => (
+    <NavLink
+        to={to}
+        className={({ isActive }) => (isActive ? "active" : undefined)}
+        style={{ textDecoration: "none" }}>
+        <StyledMenuItem {...props} />
+    </NavLink >
+);
 
 export default function Menu() {
     const [open, setOpen] = useState(false);
@@ -15,41 +64,32 @@ export default function Menu() {
     }
 
     const menuContent = (
-        <MenuList>
-            <MenuItem component={NavLink} to="/" onClick={isMobile ? toggleDrawer(false) : undefined}
-                sx={{
-                    display: 'flex', flexFlow: 'row nowrap', alignItems: 'center', gap: 1, p: 1, color: 'primary.main',
-                    '&.active': { bgcolor: 'primary.main', color: 'primary.light' }
-                }}>
+        <MenuList sx={{ p: 0 }}>
+            <NavMenuItem
+                to="/"
+                onClick={isMobile ? toggleDrawer(false) : undefined}>
                 <HomeIcon />
-                <Typography variant="body1" sx={{ fontWeight: 600 }}>Home</Typography>
-
-            </MenuItem>
-            <MenuItem component={NavLink} to="/tasks" onClick={isMobile ? toggleDrawer(false) : undefined}
-                sx={{
-                    display: 'flex', flexFlow: 'row nowrap', alignItems: 'center', gap: 1, p: 1, color: 'primary.main',
-                    '&.active': { bgcolor: 'primary.main', color: 'primary.light' }
-                }}>
+                <Typography>Home</Typography>
+            </NavMenuItem>
+            <NavMenuItem
+                to="/tasks"
+                onClick={isMobile ? toggleDrawer(false) : undefined}>
                 <TaskIcon />
-                <Typography variant="body1" sx={{ fontWeight: 600 }}>Tasks</Typography>
-            </MenuItem>
+                <Typography>Tasks</Typography>
+            </NavMenuItem>
         </MenuList>
     );
-
 
     return (
         <>
             {isMobile ? (
                 <>
-                    <IconButton
+                    <StyledIconButton
                         edge="start"
                         aria-label="menu"
-                        onClick={toggleDrawer(true)}
-                        sx={{ color: 'primary.main', position: 'fixed', top: 8, left: 20, bgcolor: 'background.paper' }}
-                    >
+                        onClick={toggleDrawer(true)}>
                         <MenuIcon />
-                    </IconButton>
-
+                    </StyledIconButton>
 
                     <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
                         <Box sx={{ width: '70vw' }} role="presentation">
@@ -58,9 +98,9 @@ export default function Menu() {
                     </Drawer>
                 </>
             ) : (
-                <Paper elevation={4} sx={{ borderRadius: 2, height: 'fit-content', mt: 1 }}>
+                <StyledPaper elevation={6}>
                     {menuContent}
-                </Paper>
+                </StyledPaper>
             )}
         </>
     );
