ae3bacccba1d7efe4893499dd54eb70713a77ef5
dtavaresm <tavares.machado.debora@gmail.com>
2025-10-01 01:15:57 +0100

    refactor(TasksPage): update TasksPage component to add useFetch hook

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,35 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useFetch from "../hooks/useFetch";
 import { Alert, Box, CircularProgress, IconButton, Pagination, Stack } from "@mui/material";
 import RefreshRoundedIcon from '@mui/icons-material/RefreshRounded';
-import { Task } from "../types";
 import TasksList from "../components/TasksList";
 
-const BASE_URL = "http://localhost:8080/api";
-
 export default function TasksPage() {
-    const [error, setError] = useState<Error | null>(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [tasks, setTasks] = useState<Task[]>([]);
+    const { error, isLoading, tasks, refetch } = useFetch();
     const [currentPage, setCurrentPage] = useState(1);
 
     const itemsPerPage = 5;
     const paginationCount = Math.ceil(tasks.length / itemsPerPage);
-
-    const fetchTasks = async () => {
-        setIsLoading(true);
-        try {
-            const response = await fetch(`${BASE_URL}/tasks`);
-            const data = await response.json();
-            setTasks(data);
-        } catch (error) {
-            setError(error instanceof Error ? error : new Error("Error"));
-        }
-        finally {
-            setIsLoading(false);
-        }
-    };
-
-    useEffect(() => { fetchTasks(); }, []);
 
     if (isLoading) {
         return (
@@ -44,7 +24,7 @@ export default function TasksPage() {
             <Alert severity="error" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', width: 'stretch' }}>
                 Something went wrong. Please try again.
                 <IconButton aria-label="delete" sx={{ ml: 2 }}>
-                    <RefreshRoundedIcon sx={{ color: 'primary.main' }} onClick={fetchTasks} />
+                    <RefreshRoundedIcon sx={{ color: 'primary.main' }} onClick={refetch} />
                 </IconButton>
             </Alert>
         )
