193c4a6a6a153c2b85b7a135502fa8fc56deaef3
dtavaresm <tavares.machado.debora@gmail.com>
2025-10-01 18:02:26 +0100

    feat(TaskCars): add event handler and dialog to delete task button

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Checkbox, FormControlLabel, FormGroup, IconButton, Paper, Typography } from '@mui/material';
+import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControlLabel, FormGroup, IconButton, Paper, Typography } from '@mui/material';
 import { Task } from "../types";
 
 type TaskCardProps = {
@@ -7,11 +8,43 @@ type TaskCardProps = {
 };
 
 export default function TaskCard({ task }: TaskCardProps) {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <Paper elevation={3} sx={{ px: 3, py: 2, borderRadius: 2, width: 'stretch', bgcolor: 'primary.light' }}>
-            <IconButton aria-label="delete" sx={{ float: 'right' }}>
+            <IconButton aria-label="delete" sx={{ float: 'right' }} onClick={handleClickOpen}>
                 <DeleteIcon sx={{ color: 'primary.main' }} />
             </IconButton>
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Continue to delete this task?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        This will permanently delete the task.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={handleClose} disabled>
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
 
             <Typography variant="body1" sx={{ color: 'primary.dark' }}>
                 {task.text}
