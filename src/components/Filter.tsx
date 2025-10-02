af210dbc86f465637f2987a043b32599dff538c2
dtavaresm <tavares.machado.debora@gmail.com>
2025-10-02 17:53:16 +0100

    feat(Filter): add Filter component to filter tasks by name and status.

---
+++ src/components/Filter.tsx
@@ -0,0 +1,42 @@
+import { Box, MenuItem, Select, TextField, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+type TasksFilterProps = {
+    searchQuery: string;
+    onSearchChange: (value: string) => void;
+    statusFilter: "all" | "completed" | "unfinished";
+    onStatusChange: (value: "all" | "completed" | "unfinished") => void;
+};
+
+export default function Filter({ searchQuery, onSearchChange, statusFilter, onStatusChange }: TasksFilterProps) {
+    return (
+        <Box sx={{ display: 'flex', width: '100%', maxWidth: '100%', flexWrap: 'wrap', gap: 2, alignItems: 'center'}}>
+            <TextField
+                label="Search tasks"
+                size="small"
+                variant="outlined"
+                value={searchQuery}
+                onChange={(e) => onSearchChange(e.target.value)}
+                slotProps={{
+                    input: {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>
+                        ),
+                    }
+                }}
+            />
+            <Select
+                size="small"
+                value={statusFilter}
+                onChange={(e) => onStatusChange(e.target.value as "all" | "completed" | "unfinished")}
+            >
+                <MenuItem value="all">All</MenuItem>
+                <MenuItem value="completed">Completed</MenuItem>
+                <MenuItem value="unfinished">Unfinished</MenuItem>
+            </Select>
+        </Box>
+
+    );
+}
